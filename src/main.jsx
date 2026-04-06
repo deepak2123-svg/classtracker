4464cad30c0dd60b2515973a1d421c33a0b9236d
deepak2123-svg <deepakkarnal321@gmail.com>
2026-04-06 19:37:12 +0530

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ClassTracker from './ClassTracker.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ClassTracker />
-  </React.StrictMode>
-)
