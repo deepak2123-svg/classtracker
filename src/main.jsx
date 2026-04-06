adc1db1094e7d81fe14ef6c910e03f906bcb2878
deepak2123-svg <deepakkarnal321@gmail.com>
2026-04-06 17:08:49 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ClassTracker from './ClassTracker.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ClassTracker />
+  </React.StrictMode>
+)
