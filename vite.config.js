1c91a4197268c35e18b90aa3e53b72d02a9541e1
deepak2123-svg <deepakkarnal321@gmail.com>
2026-06-02 19:28:38 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Target Chrome 87+ / Android WebView from ~2020 onwards.
+    // This covers older vivo/Oppo/Xiaomi devices running stock or
+    // OEM browsers that don't fully support ES2022+ syntax.
+    target: ['chrome87', 'firefox78', 'safari13', 'edge88'],
+  },
+  optimizeDeps: {
+    // Force Vite to pre-bundle tabler icons so its internal syntax
+    // is also transpiled down to the build target above.
+    include: ['@tabler/icons-react'],
+  },
 })
